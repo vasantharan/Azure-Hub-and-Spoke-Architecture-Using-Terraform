c99fcf5e796d39dbbf2c3dbfa0bd0862be3d978f
KUMARANVASANTH <kumarakurubaran944@gmail.com>
2024-01-13 04:02:48 +0530

    Vnet code is updated

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "hub-vnet" {
   name                = "Hub-vnet"
-  location            = azurerm_resource_group.Hub-And-Spoke.location
+  location            = "westus"
   resource_group_name = azurerm_resource_group.Hub-And-Spoke.name
   address_space       = ["10.1.0.0/16"]
   tags = {
