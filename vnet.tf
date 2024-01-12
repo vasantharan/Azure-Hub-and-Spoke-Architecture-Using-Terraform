bd4402674916c2d19d9796c8b166145d3c940f1d
KUMARANVASANTH <kumarakurubaran944@gmail.com>
2024-01-12 10:47:41 +0530

    Vnet code is added

--- vnet.tf
+++ vnet.tf
@@ -7,3 +7,21 @@ resource "azurerm_virtual_network" "hub-vnet" {
     environment = "dev"
   }
 }
+resource "azurerm_virtual_network" "spoke-1-vnet" {
+  name                = "spoke-1-vnet"
+  location            = azurerm_resource_group.Hub-And-Spoke.location
+  resource_group_name = azurerm_resource_group.Hub-And-Spoke.name
+  address_space       = ["10.2.0.0/16"]
+  tags = {
+    environment = "dev"
+  }
+}
+resource "azurerm_virtual_network" "spoke-2-vnet" {
+  name                = "spoke-2-vnet"
+  location            = azurerm_resource_group.Hub-And-Spoke.location
+  resource_group_name = azurerm_resource_group.Hub-And-Spoke.name
+  address_space       = ["10.3.0.0/16"]
+  tags = {
+    environment = "dev"
+  }
+}
