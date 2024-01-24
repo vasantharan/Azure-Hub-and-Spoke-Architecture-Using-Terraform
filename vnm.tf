f7706e9dbcdc21494139e6daabdeaa1d908a1a31
KUMARANVASANTH <kumarakurubaran944@gmail.com>
2024-01-24 14:39:56 +0530

    Code update

--- vnm.tf
+++
@@ -1,9 +0,0 @@
-# resource "azurerm_network_manager" "hub_and_spoke_manager" {
-#   name                = "Hub_and_spoke_net_manager"
-#   location            = azurerm_resource_group.Hub-And-Spoke.location
-#   resource_group_name = azurerm_resource_group.Hub-And-Spoke.name
-#   scope {
-#     subscription_ids = [data.azurerm_subscription.current.id]
-#   }
-#   scope_accesses = ["Connectivity", "SecurityAdmin"]
-# }
